1111aaffe824de8d1b40880150093b1028de4469
maiha <maiha@wota.jp>
2017-10-11 02:22:08 +0900

    refactor `Batch`

--- src/facebook/client/commands/batch.cr
+++ src/facebook/client/commands/batch.cr
@@ -18,33 +18,27 @@ class Facebook::Client
   # Facebook::Client.new(%(-F batch=[{"method":"GET","relative_url":"v1/me"},{"method":"GET","relative_url":"v1/act_123/campaigns"}]))
   # ```
   module Commands::Batch
-    class BatchApi
-      property array : Array(Options) = Array(Options).new
-
-      def initialize(@args : Array(String), @max : Int32 = 50)
+    private class BatchApi
+      def initialize(@max : Int32 = 50)
+        @array = Array(Options).new
       end
       
       def execute(args)
-        if array.size >= @max
+        if @array.size >= @max
           raise Errors::TooManyBatch.new("Maximum batch size is #{@max}")
         end
-        array << Options.parse!(args)
-      end
-
-      protected def execute
-        client = Client.new(@args + ["-F", "batch=#{batch_string}"])
-        client.execute
+        @array << Options.parse!(args)
       end
 
       def batch_string
-        "[%s]" % array.map(&.batch_string).join(",")
+        "[%s]" % @array.map(&.batch_string).join(",")
       end
     end
     
     def batch : HTTP::Client::Response
-      api = BatchApi.new(original_args, max: options.maxbatch)
+      api = BatchApi.new(max: options.maxbatch)
       yield api
-      api.execute
+      merge(["-F", "batch=#{api.batch_string}"]).execute
     end
   end
 end
